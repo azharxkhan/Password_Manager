91acf4037f40413d8ac1021b2ef048ec23b945cd
Azhar <azhargamer15@gmail.com>
2024-10-19 19:10:31 +0200

    move generator to src

---
+++ src/main/java/passwords/com/Generator/PasswordGenerator.java
@@ -0,0 +1,32 @@
+package passwords.com.Generator;
+
+import java.security.SecureRandom;
+
+public class PasswordGenerator {
+    private static final String CHARACTERS = "abcdefghijklmnopqrstuvwxyz0123456789";
+    private static final SecureRandom random = new SecureRandom();
+
+    public String generatePassword() {
+        StringBuilder password = new StringBuilder();
+
+        for (int i = 0; i < 3; i++) { // Generate 3 groups of 5 characters
+            for (int j = 0; j < 5; j++) {
+                char character = CHARACTERS.charAt(random.nextInt(CHARACTERS.length()));
+                if (random.nextBoolean()) {
+                    character = Character.toUpperCase(character);
+                }
+                password.append(character);
+            }
+            if (i < 2) {
+                password.append("-"); // Append hyphen between groups
+            }
+        }
+        return password.toString();
+    }
+
+    public static void main(String[] args) {
+        PasswordGenerator generator = new PasswordGenerator();
+        String password = generator.generatePassword();
+        System.out.println("Generated Password: " + password);
+    }
+}
